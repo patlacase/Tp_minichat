668a142cb2ae6b752af83026937cd74c734d1f90
pat la case <patlacase2@libertysurf.fr>
2017-05-06 09:36:57 +0200

    requete preparre pour inserer les messages dans la bdd et redirection

--- minichat_post.php
+++ minichat_post.php
@@ -4,7 +4,7 @@
 
 	try
 	{
-		$bdd = new PDO('mysql:host=localhost,dbname=test,charset=utf8'; 'root'; '');
+		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 		array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 	}
 
@@ -15,4 +15,14 @@
 		die ('Erreur' . $e -> getMessage());
 	}
 
-	
+	//insertion d'un message grâce à une requête préparée
+
+	$req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES (?, ?)');
+
+	$req->execute(array($_POST['pseudo'], $_POST['message']));
+
+	//redirection du visiteur vers la page minichat.php
+
+	header('Location: minichat.php');
+
+?>
